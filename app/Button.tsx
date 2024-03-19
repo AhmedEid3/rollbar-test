045ad70da8bfea106a291181d5234de737923152
Ahmed Eid <ahmed.eid3@outlook.com>
2024-03-19 14:07:06 +0200

    init

---
+++ app/Button.tsx
@@ -0,0 +1,33 @@
+"use client";
+export default function Button() {
+  function TestError() {
+    try {
+      const a = null;
+      return a.hello();
+    } catch (error) {
+      console.log("Error", error);
+      window.Rollbar?.error(error);
+    }
+  }
+  function TestError2() {
+    const a = null;
+    return a.hello();
+  }
+
+  return (
+    <div className="flex gap-3">
+      <button
+        className="p-2 bg-red-500 text-slate-900 rounded"
+        onClick={TestError}
+      >
+        Click me
+      </button>
+      <button
+        className="p-2 bg-blue-500 text-slate-900 rounded"
+        onClick={TestError2}
+      >
+        Click me 2{" "}
+      </button>
+    </div>
+  );
+}
