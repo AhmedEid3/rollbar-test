ce3dd1a28b6ca91eef3f1cb9433341c886374aef
Ahmed Eid <ahmed.eid3@outlook.com>
2024-03-20 00:17:27 +0200

    update page

--- app/rollbar/page.tsx
+++ app/rollbar/page.tsx
@@ -1,4 +1,15 @@
-export default function rollbar() {
+import { cookies } from "next/headers";
+
+interface RollbarProps {
+  searchParams: {
+    search: string;
+    page: number;
+  };
+}
+
+export default function rollbar({ searchParams }: RollbarProps) {
+  console.log(searchParams);
+  const user = cookies().get("user");
   throw new Error("Error from page rollbar.tsx");
   return <div>rollbar</div>;
 }
